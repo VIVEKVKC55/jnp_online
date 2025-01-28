6be490844cba7541d37513e00085fbcaacb56d82
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-29 00:51:12 +0530

    dep

--- jnp_online/settings.py
+++ jnp_online/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-v+6dk(rytib*gh*qfth$xwwnqm((#vjcf1(um+6k!0kc3$ugie"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
