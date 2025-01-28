202cbfab0d0222ecc5a2db77ce881b2602036bcb
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-28 16:20:40 +0530

    changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Category, ProductBrand, Product, ProductImages,ProductAttributes, ProductAttributeValue
+from .models import Category, ProductBrand,ProductAttributes, ProductImages
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug', 'category_group_id', 'parent', 'created', 'created_by', 'updated', 'updated_by')
+    list_display = ('id', 'name', 'slug', 'parent', 'created', 'created_by', 'updated', 'updated_by')
     search_fields = ('name', 'slug')
     list_filter = ('created', 'updated')
 
@@ -12,12 +12,6 @@ class ProductBrandAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'created', 'created_by', 'updated', 'updated_by')
     search_fields = ('name', 'slug')
     list_filter = ('created', 'updated')
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug', 'brand', 'is_deleted', 'is_enabled', 'created', 'created_by', 'updated', 'updated_by')
-    search_fields = ('name', 'slug')
-    list_filter = ('is_enabled', 'is_best_seller', 'is_home_featured', 'is_featured', 'created', 'updated')
 
 
 @admin.register(ProductImages)
@@ -36,16 +30,3 @@ class ProductAttributesAdmin(admin.ModelAdmin):
     list_filter = ('is_default',)  # Filter by default status
     search_fields = ('attribute_name',)  # Enable search by attribute name
     ordering = ('id',)  # Order by ID
-
-
-@admin.register(ProductAttributeValue)
-class ProductAttributeValueAdmin(admin.ModelAdmin):
-    """Customize the ProductPartAttributeValue admin interface."""
-    list_display = ('id', 'product', 'attribute', 'attribute_value', 'order_no')
-    list_filter = ('attribute',)  # Filter by attribute type
-    search_fields = ('product_part__name', 'attribute__attribute_name', 'attribute_value')  # Enable search
-    ordering = ('order_no',)  # Default ordering by `order_no`
-
-    autocomplete_fields = ('product', 'attribute') 
-
-
