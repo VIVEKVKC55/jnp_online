89f518c4a49a26c01a3e9a3205a397dee3df996b
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-28 21:19:02 +0530

    code improvement

--- pim/urls.py
+++ pim/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 app_name = 'pim'
+
 urlpatterns = [
     path('add/', views.ProductCreateView.as_view(), name='add'),
     path('product_list/', views.ProductListView.as_view(), name='product_list'),
