509a94bade4b6a68db4e9bbdea3de11fdec58ce9
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-29 12:50:16 +0530

    th

--- jnp_online/settings.py
+++ jnp_online/settings.py
@@ -77,10 +77,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "jnp_online.wsgi.application"
 
 
-# Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-
 
 DATABASES = {
     'default': {
