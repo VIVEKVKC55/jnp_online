6b239e517fd62c601c8561915bd80202b06f859c
Vivek Kumar <vivekvkc55@gmail.com>
2025-01-30 21:38:39 +0530

    add cat page

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     
     path('list/', views.product_list, name='product_list'),
     path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('category/<slug:slug>/', views.CategoryProductView.as_view(), name='cat_products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, ProductImages, ProductAttributeValue
+from .models import Product, Category, ProductImages, ProductAttributeValue
 from business.models import BusinessDetails
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
 
 def product_detail(request, pk):
     # Fetch the product by pk
@@ -22,3 +24,19 @@ def product_list(request):
     products = Product.objects.filter(is_deleted=False, is_enabled=True)
     return render(request, 'default/catalog/product_list.html', {'products': products})
 
+class CategoryProductView(ListView):
+    model = Product
+    template_name = 'default/catalog/cat_products.html'
+    context_object_name = 'products'
+    paginate_by = 10  # Optional: Add pagination
+
+    def get_queryset(self):
+        """Get products for a specific category."""
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Product.objects.filter(category=self.category, is_enabled=True, is_deleted=False)
+
+    def get_context_data(self, **kwargs):
+        """Add category object to context."""
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
