a5505e43a81cbb6eaadc043795033d8ef806d0b2
Vivek Kumar <vivekvkc55@gmail.com>
2025-02-11 13:26:54 +0530

    improve theem

--- home/views.py
+++ home/views.py
@@ -3,12 +3,9 @@ from catalog.models import Product, ProductImages
 
 def home(request):
     # Fetch all products with their associated categories
-    featured_products = Product.objects.filter(is_deleted=False, is_home_featured=True, is_enabled=True).order_by('-created')
-
     products = Product.objects.filter(is_deleted=False, is_enabled=True, is_approved=True).order_by('-created')
     
     context = {
         'products': products,
-       'featured_products': featured_products
     }
     return render(request, 'default/home/index.html', context)
